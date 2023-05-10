9598f0fae02f98ba4b9f3483e4dc1e1bb8f58815
Akindejoye <tydruhill@gmail.com>
2023-05-10 13:10:22 +0100

    Theme setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useMemo } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
+
 const App = () => {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => 
+    createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
-      <h1>Billion Dashboard</h1>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <h1>Billion Dashboard</h1>
+      </ThemeProvider>
     </div>
   );
 };
