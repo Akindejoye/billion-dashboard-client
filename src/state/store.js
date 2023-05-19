1187ceed629852f781ca84a7ba475642bfd97cca
Akindejoye <tydruhill@gmail.com>
2023-05-19 12:33:37 +0100

    Separating State to state file

---
+++ src/state/store.js
@@ -0,0 +1,13 @@
+import globalReducer from "state";
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { api } from "state/api";
+
+export const store = configureStore({
+  reducer: {
+    global: globalReducer,
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
+});
+setupListeners(store.dispatch);
